e80d7c5ebb869f52574c84fd769e7fbe8621d4dd
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-12-31 16:20:16 +0530

    Reset project

--- projects/components/src/public-api.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Public API Surface of components
- */
-
-export * from './lib/components.service';
-export * from './lib/components.component';
-export * from './lib/components.module';
-export * from './lib/multiselect/multiselect.module';
-export * from './lib/multiselect/multiselect.component';
